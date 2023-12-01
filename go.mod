dbd0032eeaf28a1f6d3e7b53fc65d1f5558f6cc1
narveer-loyalist <narveersaharan@loyalistcollege.com>
2023-11-30 20:27:59 -0500

    changes

--- go.mod
+++ go.mod
@@ -6,7 +6,11 @@ require (
 	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.18 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
+	gorm.io/driver/sqlite v1.5.4 // indirect
+	gorm.io/gorm v1.25.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,50 +1,97 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+const dbPath = "mydatabase.db"
+
+// TimeLog represents the structure of the time_log table
+type TimeLog struct {
+	ID        uint      `gorm:"primaryKey"`
+	Timestamp time.Time `gorm:"column:timestamp"`
+}
+
+// TimeResponse represents the response structure
 type TimeResponse struct {
-	TorontoTime string `json:"toronto_time"`
+	CurrentTime string `json:"current_time"`
+}
+
+var db *gorm.DB
+
+func init() {
+	// Initialize the database connection with gorm for SQLite3
+	var err error
+	db, err = gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error opening database connection:", err)
+	}
+
+	// Auto migrate the TimeLog struct to the database
+	if err := db.AutoMigrate(&TimeLog{}); err != nil {
+		log.Fatal("Error auto migrating tables:", err)
+	}
+
+	// Check if the database connection is successful
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Error getting DB instance:", err)
+	}
+	if err = sqlDB.Ping(); err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+}
+
+func getTime(w http.ResponseWriter, r *http.Request) {
+	// Set the timezone to Toronto
+	loc, err := time.LoadLocation("America/Toronto")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error loading time zone: %v", err)
+		return
+	}
+
+	// Get the current time in Toronto
+	currentTime := time.Now().In(loc)
+
+	// Save the current time to the database using gorm
+	timeLog := TimeLog{Timestamp: currentTime}
+	if err := db.Create(&timeLog).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error inserting time into the database: %v", err)
+		return
+	}
+
+	// Create a response struct
+	response := TimeResponse{
+		CurrentTime: currentTime.Format("2006-01-02 15:04:05"),
+	}
+
+	// Set the content type to JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Encode the struct to JSON and write the response
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("Error encoding JSON response: %v", err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/time", timeHandler)
-	http.ListenAndServe(":8080", nil)
-}
+	http.HandleFunc("/time", getTime)
+	port := ":7575"
+	fmt.Printf("Server is running on port %s\n", port)
 
-func timeHandler(w http.ResponseWriter, r *http.Request) {
-	torontoTime := getCurrentTorontoTime()
-	//saveTimeToDatabase(torontoTime)
-
-	response := TimeResponse{TorontoTime: torontoTime.Format(time.RFC3339)}
-	json.NewEncoder(w).Encode(response)
-}
-
-func getCurrentTorontoTime() time.Time {
-	loc, _ := time.LoadLocation("America/Toronto")
-	return time.Now().In(loc)
-}
-
-func saveTimeToDatabase(time time.Time) {
-	db, err := sql.Open("sqlite3", "./database.db")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS time_table (id INTEGER PRIMARY KEY AUTOINCREMENT, time DATETIME)")
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = db.Exec("INSERT INTO time_table (time) VALUES (?)", time)
-	if err != nil {
-		panic(err)
+	// Start the server
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatalf("Error starting the server: %v", err)
 	}
 }
